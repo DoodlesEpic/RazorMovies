50117f3871c4d3b70796235ad146bfa05ea4c685
Doodles3 <eduardomoraes84@yahoo.com.br>
2020-11-23 18:03:58 -0300

    Update movie model with better schema

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,6 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorMovies.Models
 {
@@ -9,9 +9,13 @@ namespace RazorMovies.Models
         public int ID { get; set; }
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
