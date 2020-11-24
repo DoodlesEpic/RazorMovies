80e5f6c224bc0cfb4a8de2d590fd24efb21c38f5
Doodles3 <eduardomoraes84@yahoo.com.br>
2020-11-23 21:39:56 -0300

    Add search for movies by genre functionallity

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -35,19 +35,29 @@ namespace RazorMovies.Pages.Movies
 
         public async Task OnGetAsync()
         {
+            // Get the list of queryable genres using System.Linq
+            // We will use this to make the genre selection available to the user
+            IQueryable<string> genreQuery = from m in _context.Movie orderby m.Genre select m.Genre;
+
             // Get all the movies using System.Linq
             // The query is only defined at this point, it has not been run against the database
             var movies = from m in _context.Movie select m;
 
             // If there is a search query, then filter the movie list
-            // The query is still only defined at this point
             if (!string.IsNullOrEmpty(SearchString))
             {
                 // On SQL Server, Contains maps to SQL LIKE
                 movies = movies.Where(s => s.Title.Contains(SearchString));
             }
 
+            // If there is a selected genre, then filter the movie list
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+
             // Run the query against the database
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
             Movie = await movies.ToListAsync();
         }
     }
